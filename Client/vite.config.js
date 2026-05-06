98a0598efa1b7d3683cebd8d2dad00f598103d87
mneikoe <raj117557@gmail.com>
2026-05-06 15:45:47 +0530

    Live

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://vpf.amnrj.in",
-        //target: "http://localhost:8080",
+        //target: "http://localhost:6060",
         changeOrigin: true,
         secure: false,
       },
